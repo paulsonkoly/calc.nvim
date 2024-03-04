daacf9d1d7ea0383b323e8b7069af3c16472b350
Paul Sonkoly <sonkoly.pal@gmail.com>
2024-03-04 15:11:18 +0000

    added highlight scm

---
+++ queries/calc/highlights.scm
@@ -0,0 +1,39 @@
+(int_lit) @number
+(float_lit) @number.float
+(bool_lit) @boolean
+(string_lit) @string
+
+(kwwhile) @keyword.repeat
+[
+  (kwif)
+  (kwelse)
+] @keyword.conditional
+(kwreturn) @keyword.return
+
+[
+ (relationalop)
+ (logicop)
+ (addsubop) 
+ (divmulop)
+ (unaryop)
+ (arrowop)
+ (assignop)
+] @operator
+
+[
+ (delim_obrace)
+ (delim_cbrace)
+ (delim_oparen)
+ (delim_cparen)
+ (delim_obracket)
+ (delim_cbracket)
+] @puncation.bracket
+
+[
+ (delim_comma)
+ (delim_colon)
+] @puncation.delimiter
+
+(variable_name) @variable
+
+(ERROR) @error
